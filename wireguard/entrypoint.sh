7658b1a2cca6955f8c55f46cc71dbf9109c4cad0
Emiya <subaru.xia@zoho.com>
2018-12-13 17:29:02 +0800

    Update entrypoint.sh

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -22,36 +22,6 @@ if ! compgen -G "/etc/wireguard/*.conf" > /dev/null; then
     chmod -v 600 /etc/wireguard/wg0.conf
 fi
 
-# Install Wireguard. This has to be done dynamically since the kernel
-# module depends on the host kernel version.
-apt update
-apt install -y linux-headers-$(uname -r)
-apt install -y wireguard
-
-# Find a Wireguard interface
-interfaces=`find /etc/wireguard -type f`
-if [[ -z $interfaces ]]; then
-    echo "$(date): Interface not found in /etc/wireguard" >&2
-    exit 1
-fi
-
-interface=`echo $interfaces | head -n 1`
-
-echo "$(date): Starting Wireguard"
-wg-quick up $interface
-
-# Handle shutdown behavior
-finish () {
-    echo "$(date): Shutting down Wireguard"
-    wg-quick down $interface
-    exit 0
-}
-
-trap finish SIGTERM SIGINT SIGQUIT
-
-sleep infinity &
-wait $!
-
 # Configure my network, I think.. I just copied those..
 
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -60,13 +30,13 @@ iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT
 
-iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s ${interface_addr}/24 -o eth0 -j MASQUERADE
 
-# start the insterface
+# Start the Insterface
 wg-quick up wg0
 
 
-# check if Wireguard is running
+# Check if Wireguard is Running
 if [[ $(wg) ]]
 then
     syslogd -n      # keep container alive
