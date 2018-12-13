0c10a3eab52aecfac90c54e7b629caf0ba35786c
Emiya <subaru.xia@zoho.com>
2018-12-13 16:51:50 +0800

    Update entrypoint.sh

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,6 +2,25 @@
 
 set -e
 
+cd /etc/wireguard
+
+# Generate Server Private Keys
+
+if ! compgen -G "/etc/wireguard/*.conf" > /dev/null; then
+    echo "no config file at /etc/wireguard/*.conf – creating demo config"
+    umask 077
+    wg genkey | tee server_private_key | wg pubkey > server_public_key
+    awk '/PrivateKey/ { print; print "ListenPort = 38945"; next }1' demo.conf > wg0.conf
+    sed -i "s/0\.0\.0\.0\/0/192.168.4.1\/24/g" wg0.conf
+fi
+
+
+
+server_pvtkey=$(cat server_private_key)
+$interface_address
+
+
+
 # Install Wireguard. This has to be done dynamically since the kernel
 # module depends on the host kernel version.
 apt update
