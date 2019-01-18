4d423792ec8b8c63ff23e74a10356568b8bf090e
Emiya <xiafan@live.com>
2019-01-19 00:28:55 +0800

    fix error

--- freeradius/entrypoint.sh
+++ freeradius/entrypoint.sh
@@ -29,11 +29,11 @@ server site_ldap {
 _EOF_
 
 # Change LDAP Config
-sed -i "s/motofans.club/${LDAP_SERVER}/g" /etc/raddb/mods-available/ldap
-sed -i "s/cn=admin,dc=motofans,dc=club/${BIND_DN}/g" /etc/raddb/mods-available/ldap
-sed -i "s/xxxxxxxxxx/${PASSWD}/g" /etc/raddb/mods-available/ldap
+sed -i "s/ldapserver/${LDAP_SERVER}/g" /etc/raddb/mods-available/ldap
+sed -i "s/ldapuser/${BIND_DN}/g" /etc/raddb/mods-available/ldap
+sed -i "s/ldappwd/${PASSWD}/g" /etc/raddb/mods-available/ldap
 sed -i "s/389/${PORT}/g" /etc/raddb/mods-available/ldap
-sed -i "s/dc=motofans,dc=club/${BASE_DN}/g" /etc/raddb/mods-available/ldap
+sed -i "s/ldapbasedn/${BASE_DN}/g" /etc/raddb/mods-available/ldap
 
 # Enable Config
 ln -s /etc/raddb/mods-available/ldap /etc/raddb/mods-enabled/
