b3f40795fc2a7c0e02218f5c2f83cc2a28502f7f
Emiya <xiafan@live.com>
2019-01-18 23:52:56 +0800

    Add LDAP

---
+++ freeradius/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -xe
+
+# Create Sites Config
+cd /etc/raddb/sites-available
+
+cat >ldap <<_EOF_
+server site_ldap { 
+    listen { 
+         ipaddr = 0.0.0.0
+         port = 1833
+         type = auth
+    } 
+    authorize {
+         update {
+             control:Auth-Type := ldap
+         }
+    }
+    authenticate {
+        Auth-Type ldap {
+            ldap
+        }
+    }
+    post-auth {
+        Post-Auth-Type Reject {
+        }
+    }
+}
+_EOF_
+
+# Change LDAP Config
+sed -i "s/motofans.club/${LDAP_SERVER}/g" /etc/raddb/mods-available/ldap
+sed -i "s/cn=admin,dc=motofans,dc=club/${BIND_DN}/g" /etc/raddb/mods-available/ldap
+sed -i "s/xxxxxxxxxx/${PASSWD}/g" /etc/raddb/mods-available/ldap
+sed -i "s/389/${PORT}/g" /etc/raddb/mods-available/ldap
+sed -i "s/dc=motofans,dc=club/${BASE_DN}/g" /etc/raddb/mods-available/ldap
+
+radiusd -xxf
